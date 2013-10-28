e2dd0e56045d5076e52d95de9a42f59e7d395b39
tengcomplex <tengcomplex@gmail.com>
2013-10-28 06:58:46 +0100

    - Organize imports.

--- src/com/drinschinz/rssamantha/DocumentReader.java
+++ src/com/drinschinz/rssamantha/DocumentReader.java
@@ -32,7 +32,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
