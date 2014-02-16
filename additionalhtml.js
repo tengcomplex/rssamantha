b434224faedc6ac9bc2e78cc15dbd5b340133f08
Teng Complex <tengcomplex@gmail.com>
2014-02-16 10:01:18 +0100

    - Add additional html demo files.

---
+++ additionalhtml.js
@@ -0,0 +1,34 @@
+// Array list of days.
+var days = new Array('Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday');
+ 
+// Array list of months.
+var months = new Array('January','February','March','April','May','June','July','August','September','October','November','December');
+
+function startTime()
+{
+	var today = new Date();
+	document.getElementById('txtdate').innerHTML="Date: "+days[today.getDay()] + ", "+months[today.getMonth()]+" "+zeroPre(today.getDate())+", "+(fourdigits(today.getYear()));
+	document.getElementById('txttime').innerHTML="Time: "+zeroPre(today.getHours())+":"+zeroPre(today.getMinutes())+":"+zeroPre(today.getSeconds());
+	t=setTimeout(function(){startTime()},1000);
+}
+
+function zeroPre(i)
+{
+	return (i < 10) ? i = "0" + i : i;
+}
+
+// Calculate four digit year.
+function fourdigits(number) 
+{
+    return (number < 1000) ? number + 1900 : number;
+}
+
+var counter = 100;
+
+function countDown()
+{	t = setTimeout(function(){countDown()},1000);
+	document.getElementById('txtcountdown').innerHTML = "Next reload in "+counter+"s";
+	counter -= 1;
+}
+
+
