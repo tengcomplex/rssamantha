a216ac90ee9dc37e3c3d975633fa44adb5461af1
Teng Complex <tengcomplex@gmail.com>
2014-06-19 14:14:10 +0200

    - Organize imports.

--- src/com/drinschinz/rssamantha/DownloadControl.java
+++ src/com/drinschinz/rssamantha/DownloadControl.java
@@ -24,7 +24,6 @@ package com.drinschinz.rssamantha;
 
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Map;
