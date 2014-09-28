0a120d7be9ddf545fc6e4ecd23644c8bf569266d
Teng Complex <tengcomplex@gmail.com>
2014-09-28 15:14:52 +0200

    - Use  === and !== operators. - Cleanup.

--- src/com/drinschinz/rssamantha/res/checkInput.js
+++ src/com/drinschinz/rssamantha/res/checkInput.js
@@ -13,7 +13,7 @@ function isValidDateTime(dt)
 {
     if(!dt || !dtRegex.test(dt))
     {
-            return false;
+        return false;
     }	
     dt = dt.toString();
     // split
@@ -24,31 +24,29 @@ function isValidDateTime(dt)
     date[1] = parseInt(date[1],10)-1;//month
     time[0] = parseInt(time[0],10);
     time[1] = parseInt(time[1],10);	
-    time[2] = parseInt(time[2],10);		
-    // create and check
+    time[2] = parseInt(time[2],10);
     var cdt = new Date(date[0],date[1],date[2],time[0],time[1],time[2],0);
-    //compare
-    return cdt.getDate() == date[2] && cdt.getMonth() == date[1] && cdt.getFullYear() == date[0] && cdt.getHours() == time[0] && cdt.getMinutes() == time[1] && cdt.getSeconds() == time[2];
+    return cdt.getDate() === date[2] && cdt.getMonth() === date[1] && cdt.getFullYear() === date[0] && cdt.getHours() === time[0] && cdt.getMinutes() === time[1] && cdt.getSeconds() === time[2];
 }
 
 function checkInput() 
 {  
     var ni = document.generate.numitems.value;
-    if(ni.toUpperCase() != "ALL" && !isInt(ni)) 
+    if(ni.toUpperCase() !== "ALL" && !isInt(ni)) 
     {
         alert("Input \"Number of items\" must be \"ALL\" or an integer.");
         document.generate.numitems.focus();
         return false;
     }
     var ct = document.generate.cutoff.value;
-    if(ct != "" && ct.toUpperCase() != "TODAY" && !isInt(ct) && !isValidDateTime(ct)) 
+    if(ct !== "" && ct.toUpperCase() !== "TODAY" && !isInt(ct) && !isValidDateTime(ct)) 
     {
         alert("Input \"Cutoff\" must be empty, \"TODAY\", \"yyyy-mm-dd hh:mm:ss\" or milliseconds since epoch.");
         document.generate.cutoff.focus();
         return false;
     }
     var rf = document.generate.refresh.value;
-    if(rf != "" && !isInt(rf)) 
+    if(rf !== "" && !isInt(rf)) 
     {
         alert("Input \"Refresh\" must be an integer.");
         document.generate.refresh.focus();
