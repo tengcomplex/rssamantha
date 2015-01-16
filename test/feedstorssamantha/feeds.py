c65acdea636f2e59e3743004ca698d2e83aa3ebb
teng <teng@walker>
2015-01-16 16:18:54 +0100

    - Add python feedparser package based tool for reading broken feeds.

---
+++ test/feedstorssamantha/feeds.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+#
+#  Feeds to RSSamantha.
+#  Copyright (C) 2011-2015  David Schröer <tengcomplexATgmail.com>
+#
+#
+#  This file is part of RSSamantha.
+#
+#  RSSamantha is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  RSSamantha is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with RSSamantha.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+#
+# Usage: >python feeds.py feeds.opml
+#
+import sys
+from fetch import handleFeed
+import xml.dom.minidom
+from time import sleep
+import random
+
+RSSAMANTHA_URL = "http://localhost:8081/"
+
+if not len(sys.argv) == 2:
+	print "Error: no opml feedfilename given."
+	sys.exit(1)
+feedFileName = sys.argv[1]
+print "Reading feed from "+feedFileName
+#
+# Do work
+#
+DOMTree = xml.dom.minidom.parse(feedFileName)
+collection = DOMTree.documentElement
+channels = collection.getElementsByTagName("outline")
+for channel in channels:
+	channelName = channel.getAttribute("config.name")
+	print "*****Channel*****"
+	print "Channelname: %s" % channelName	
+	feeds = channel.getElementsByTagName("source")
+	for feed in feeds:
+		title = str(feed.getAttribute("title").encode('utf-8'))
+		url = str(feed.getAttribute("feedUrl"))
+		print "Title: "+title+" Url: "+url
+		sleep(random.randint(3,11))
+		handleFeed(RSSAMANTHA_URL, title, url, channelName)
+
---
+++ test/feedstorssamantha/fetch.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+#
+#  Feeds to RSSamantha.
+#  Copyright (C) 2011-2015  David Schröer <tengcomplexATgmail.com>
+#
+#
+#  This file is part of RSSamantha.
+#
+#  RSSamantha is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  RSSamantha is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with RSSamantha.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+#
+# Usage: >python fetch.py rssamanthaurl feedname "url" channelname
+#
+
+import sys
+import time
+import urllib
+import urllib2
+import feedparser
+
+def handleFeed(rssamanthaUrl, feedName, url, channel):
+	print "Parsing url:"+url+" ..."
+	d = feedparser.parse(url)
+	if hasattr(d['feed'], 'title') and not (d['feed']['title'] is None):
+			feedTitle = d['feed']['title']
+	else:
+			feedTitle = "n/a" 
+	print "Parsed url:"+url+" feedtitle:"+feedTitle.encode('ascii', 'ignore')+"\n"
+	for item in d.entries:
+		if hasattr(item, 'published_parsed') and not (item.published_parsed is None):
+			dt = str(int(time.mktime(item.published_parsed)*1000))
+		else:
+			dt = ""
+		if hasattr(item, 'published'):
+			p = item.published
+		else:
+			p = ""
+		if hasattr(item, 'description'):
+			desc = item.description
+		else:
+			desc = "n/a"
+		print "title:"+item.title.encode('ascii', 'ignore')+" published:["+dt+" "+p+"] link:"+item.link.encode('ascii', 'ignore')+" description:"+desc.encode('ascii', 'ignore')
+		ret = sendItem(rssamanthaUrl, ("["+feedName.decode('utf-8')+"] "+item.title), dt, desc, item.link, channel)
+		print ret
+	print "--------------------\n"
+
+def sendItem(rssamanthaUrl, title, d, description, link, channel):
+	values = {
+		"title" :  title.encode('utf-8'),
+		"channel0" : channel,
+		"description" : description.encode('utf8'),
+		"link" : link.encode('utf8')
+	}
+	if len(d) > 0:
+		values["created"] = d
+
+	data = urllib.urlencode(values)
+	req = urllib2.Request(rssamanthaUrl, data)
+	try:
+		response = urllib2.urlopen(req)
+		r = response.read()
+		return r
+	except urllib2.HTTPError, err:
+		return "HTTP Error "+str(err.code)
+#
+# Do work if called standalone, with arguments
+#
+if len(sys.argv) == 5:
+	handleFeed(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+
