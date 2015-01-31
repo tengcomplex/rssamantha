07e1274da5fd66a704980faee0fb029f940ea731
teng <teng@walker>
2015-01-31 18:13:27 +0100

    - Update copyright.

--- test/bingtraffictorssamantha/bingtraffictorssamantha.py
+++ test/bingtraffictorssamantha/bingtraffictorssamantha.py
@@ -1,6 +1,6 @@
 #
 #  Bing to RSSamantha.
-#  Copyright (C) 2011-2014  David Schröer <tengcomplexATgmail.com>
+#  Copyright (C) 2011-2015  David Schröer <tengcomplexATgmail.com>
 #
 #
 #  This file is part of RSSamantha.
